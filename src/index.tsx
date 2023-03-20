f9419ebc30b50056a2eb14d7520f2622ccc515a0
Massimo Fazari <massimofazari@massimos-mbp.home>
2023-03-20 23:09:17 +0000

    Add Primitive Routing Debug

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from './store/finance/store';
 import { Provider } from 'react-redux';
-import Home from "./views/Home";
-import Chart from "./views/Chart";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -27,8 +23,4 @@ root.render(
     </Provider>
     </>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
